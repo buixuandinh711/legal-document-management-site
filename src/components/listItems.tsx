0dade853ca02a980fed4e8963563d016b2880a81
buixuandinh711 <buixuandinh711@gmail.com>
2023-10-10 00:12:57 +0700

    add basic route

--- src/components/listItems.tsx
+++ src/components/listItems.tsx
@@ -1,47 +1,50 @@
-import * as React from 'react';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import ListSubheader from '@mui/material/ListSubheader';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import PeopleIcon from '@mui/icons-material/People';
-import BarChartIcon from '@mui/icons-material/BarChart';
-import LayersIcon from '@mui/icons-material/Layers';
-import AssignmentIcon from '@mui/icons-material/Assignment';
+import * as React from "react";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import ListSubheader from "@mui/material/ListSubheader";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import { Link } from "react-router-dom";
+import { Grading, LibraryBooks, NoteAlt } from "@mui/icons-material";
 
 export const mainListItems = (
   <React.Fragment>
-    <ListItemButton>
-      <ListItemIcon>
-        <DashboardIcon />
-      </ListItemIcon>
-      <ListItemText primary="Dashboard" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <ShoppingCartIcon />
-      </ListItemIcon>
-      <ListItemText primary="Orders" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <PeopleIcon />
-      </ListItemIcon>
-      <ListItemText primary="Customers" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <BarChartIcon />
-      </ListItemIcon>
-      <ListItemText primary="Reports" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <LayersIcon />
-      </ListItemIcon>
-      <ListItemText primary="Integrations" />
-    </ListItemButton>
+    <Link to="/dashboard" style={{ textDecoration: "none", color: "inherit" }}>
+      <ListItemButton>
+        <ListItemIcon>
+          <DashboardIcon />
+        </ListItemIcon>
+        <ListItemText primary="Dashboard" />
+      </ListItemButton>
+    </Link>
+    <Link to="/drafting" style={{ textDecoration: "none", color: "inherit" }}>
+      <ListItemButton>
+        <ListItemIcon>
+          <NoteAlt />
+        </ListItemIcon>
+        <ListItemText primary="Drafting" />
+      </ListItemButton>
+    </Link>
+    <Link to="/reviewing" style={{ textDecoration: "none", color: "inherit" }}>
+      <ListItemButton>
+        <ListItemIcon>
+          <Grading />
+        </ListItemIcon>
+        <ListItemText primary="Reviewing" />
+      </ListItemButton>
+    </Link>
+    <Link
+      to="/manage-document"
+      style={{ textDecoration: "none", color: "inherit" }}
+    >
+      <ListItemButton>
+        <ListItemIcon>
+          <LibraryBooks />
+        </ListItemIcon>
+        <ListItemText primary="Manage Docs" />
+      </ListItemButton>
+    </Link>
   </React.Fragment>
 );
 
@@ -50,23 +53,38 @@ export const secondaryListItems = (
     <ListSubheader component="div" inset>
       Saved reports
     </ListSubheader>
-    <ListItemButton>
-      <ListItemIcon>
-        <AssignmentIcon />
-      </ListItemIcon>
-      <ListItemText primary="Current month" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <AssignmentIcon />
-      </ListItemIcon>
-      <ListItemText primary="Last quarter" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <AssignmentIcon />
-      </ListItemIcon>
-      <ListItemText primary="Year-end sale" />
-    </ListItemButton>
+    <Link
+      to="/current-month"
+      style={{ textDecoration: "none", color: "inherit" }}
+    >
+      <ListItemButton>
+        <ListItemIcon>
+          <AssignmentIcon />
+        </ListItemIcon>
+        <ListItemText primary="Current month" />
+      </ListItemButton>
+    </Link>
+    <Link
+      to="/last-quarter"
+      style={{ textDecoration: "none", color: "inherit" }}
+    >
+      <ListItemButton>
+        <ListItemIcon>
+          <AssignmentIcon />
+        </ListItemIcon>
+        <ListItemText primary="Last quarter" />
+      </ListItemButton>
+    </Link>
+    <Link
+      to="/year-end-sale"
+      style={{ textDecoration: "none", color: "inherit" }}
+    >
+      <ListItemButton>
+        <ListItemIcon>
+          <AssignmentIcon />
+        </ListItemIcon>
+        <ListItemText primary="Year-end sale" />
+      </ListItemButton>
+    </Link>
   </React.Fragment>
 );
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,26 @@
+import { Typography } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Dashboard from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "src/components/Layout";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Dashboard />
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route
+            path="/"
+            element={<Typography variant="h1">Root</Typography>}
+          />
+          <Route
+            path="/about"
+            element={<Typography variant="h1">About</Typography>}
+          />
+          <Route path="*" element={<Typography variant="h1">404 Not Found</Typography>} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+    ,
   </React.StrictMode>
 );
